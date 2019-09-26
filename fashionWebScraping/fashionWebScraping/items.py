368c2a185e9edb85790b37c8ae1efcb07ef13126
Erdem İşbilen <erdemisbilen@gmail.com>
2019-09-26 23:30:10 +0100

    Add files via upload

---
+++ fashionWebScraping/fashionWebScraping/items.py
@@ -0,0 +1,26 @@
+
+import scrapy
+from scrapy.item import Item, Field
+
+class FashionwebscrapingItem(scrapy.Item):
+ 
+    #product related items
+    gender=Field()
+    productId=Field()
+    productName=Field()
+    priceOriginal=Field()
+    priceSale=Field()
+
+    #items to store links 
+    imageLink = Field()
+    productLink=Field()
+
+    #item for company name
+    company = Field()
+
+    #items for image pipeline
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
+
+
+    pass
